bfc3d8389e13626a2d15ca2b51b7faa233616ad0
aluisio <aaluisio309@gmail.com>
2024-04-20 10:51:29 -0300

    sistema 1

--- numberOne.py
+++ numberOne.py
@@ -1,11 +1,9 @@
-menu = """
-   Qual operação deixa fazer?
-     [1] sacar
-     [2] depositar
-     [3] ver extrato
-     [0] sair
-
-     -->
+menu = """ Qual operação deixa fazer?
+[1] sacar
+[2] depositar
+[3] ver extrato
+[0] sair
+---->
 """
 
 
@@ -28,44 +26,52 @@ while True:
             valor_do_saque = input("quanto deseja sacar:")
            
             if int(valor_do_saque) <= LIMITE_DIARIO_VALOR_SAQUE and int(valor_do_saque) <= saldo and int(valor_do_saque) >= 0:
-                print("saque concluido")
+                print("saque concluido\n")
                 saques += 1
                 saldo -= int(valor_do_saque)
-                print(f"voce sacou {float(valor_do_saque)} da sua conta ")
+                print(f"voce sacou {float(valor_do_saque)} da sua conta \n")
                 estrato_bancario += f"saque de {float(valor_do_saque)}\n"
            
             elif  int(valor_do_saque) > LIMITE_DIARIO_VALOR_SAQUE :
-                print(f"seu limete diario de saque e somente de R${float(LIMITE_DIARIO_VALOR_SAQUE)}")  
+                print('operacao invalida\n')
+                print(f"seu limete diario de saque e somente de R${float(LIMITE_DIARIO_VALOR_SAQUE)}\n")  
            
             elif  int(valor_do_saque) >= saldo :
+                print('operacao invalida\n')
                 print(f"""voce nao tem saldo suficiente para esse saque
-Seu saldo e de {float(saldo)}""")   
+Seu saldo e de {float(saldo)}\n""")   
             else : 
-                print('operacao invalida')
+                print('operacao invalida\n')
 
         else :
-            print(f"voce ja excedeu o limete de saques diario de {LIMITE_DE_SAQUE_DIARIO}")        
+            print(f"voce ja excedeu o limete de saques diario de {LIMITE_DE_SAQUE_DIARIO}\n")        
    
    
     elif opcao == "2":
-        print('depositor')
         valor_do_deposito = input("quanto deseja depositar:")
-        saldo += int(valor_do_deposito)
-        print(f"voce depositou {float(valor_do_deposito)} na sua conta ")
-        estrato_bancario += f"depositor de {float(valor_do_deposito)}\n"
+
+        if int(valor_do_deposito) >= 0:
+            saldo += int(valor_do_deposito)
+            print(f"voce depositou {float(valor_do_deposito)} na sua conta \n")
+            estrato_bancario += f"depositor de {float(valor_do_deposito)}\n"
+        else:
+            print('operacao invalida\n')
+        
        
 
     elif  opcao == "3":
         print(f"\n####### EXTRATO ###### ")
+        print()
         print(f"Nao foram realizado nenhuma operacao ate agora." if not estrato_bancario else estrato_bancario)
         print(f"seu saldo e de {saldo:.2f}")
-        print('##########################################')
+        print()
+        print('######################\n')
 
     elif opcao == "0":
         break
 
     else :
-        print('escolha um operacao')
+        print('escolha um operacao\n')
 
             
 
